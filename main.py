8dd4119ee85dd5b6f329823c8fd11ee9d4d8702b
Jaedan <jaedanparsons@gmail.com>
2024-07-18 10:32:34 -0800

    Company employee website page

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas
+
+st.title("Company Website")
+st.write("Here is the description of the company. Blah blah blah")
+
+col1, brkc1, col2, brekc2, col3 = st.columns([1,.25,1,.25,1])
+
+data = pandas.read_csv("data.csv", sep=';')
+
+num_employees = len(data)
+
+num_col1 = num_employees/3 + num_employees%3
+num_col2 = num_employees/3
+if num_employees%3 > 1:
+    num_col2 += 1
+
+with col1:
+    for index, row in data[:num_col1].iterrows():
+        st.header(row["first name"] + row["last name"])
+        st.write(row["role"])
+        st.image("images/" + row['image'])
+
+with col2:
+    for index, row in data[num_col1:num_col2].iterrows():
+        st.header(row["first name"] + row["last name"])
+        st.write(row["role"])
+        st.image("images/" + row['image'])
+
+with col3:
+    for index, row in data[num_col2:].iterrows():
+        st.header(row["first name"] + row["last name"])
+        st.write(row["role"])
+        st.image("images/" + row['image'])
